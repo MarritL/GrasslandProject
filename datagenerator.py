c9aeeec3d1fcaf5363daf8eb06d589b93f163087
MarritL <marritleenstra@hotmail.com>
2019-10-02 16:29:12 +0200

    test with edge detection

--- datagenerator.py
+++ datagenerator.py
@@ -61,12 +61,12 @@ class DataGen(keras.utils.Sequence):
         # edge detection instead of classes
         if self.n_classes == 2:
             y_full = y
-            y = np.zeros((self.batch_size, self.max_size, self.max_size, 1), dtype=np.int8)
+            y = np.zeros((self.batch_size, self.max_size, self.max_size, 2), dtype=np.int8)
             for i in range(self.batch_size):
                 gt_classes = np.argmax(y_full[i,], axis=2)
                 edges = ndimage.sobel(gt_classes)
                 edges[edges !=0] = 1
-                y[i,] = keras.utils.to_categorical(gt_classes)
+                y[i,] = to_categorical_classes(gt_classes, [0,1])
         
         return X,y
     
